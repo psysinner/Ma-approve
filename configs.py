396e0d6d9b2a0eb4037326d0dd96eba90cae4978
psysinner <psychosinner@proton.me>
2025-07-29 02:16:24 +0530

    Add files via upload

---
+++ configs.py
@@ -0,0 +1,12 @@
+from os import path, getenv
+
+class Config:
+    API_ID = int(getenv("API_ID", "0112234"))
+    API_HASH = getenv("API_HASH", "abcdefg")
+    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
+    FSUB = getenv("FSUB", "SDBotz")
+    CHID = int(getenv("CHID", "-1000112234"))
+    SUDO = list(map(int, getenv("SUDO","5023815012 6184402222 6387145248 1877279215 6067718572").split()))
+    MONGO_URI = getenv("MONGO_URI", "")
+    
+cfg = Config()
