b78f7a5665b04c3c53a3f2d1b93f44a870af8260
psysinner <psychosinner@proton.me>
2025-07-29 02:18:25 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,4 +1,8 @@
-from os import path, getenv
+from os import getenv
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 class Config:
     API_ID = int(getenv("API_ID", "0112234"))
@@ -6,7 +10,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
     FSUB = getenv("FSUB", "SDBotz")
     CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO","5023815012 6184402222 6387145248 1877279215 6067718572").split()))
+    SUDO = list(map(int, getenv("SUDO", "5023815012 6184402222 6387145248 1877279215 6067718572").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
